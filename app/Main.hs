beae884397e5a356a4b65e6c08aeae9f3169831a
Takumi Crary <tcrary@gmail.com>
2021-04-21 03:45:41 +0100

    Split out low level ID3 stuff out into the library

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,8 @@
-{-# LANGUAGE DeriveGeneric #-}
-
 module Main where
 
-import Lib
+import Wamphf
 
-import GHC.Generics
 import Data.Aeson
-import qualified ID3.Simple as ID3
-
 import Options.Applicative
 import System.Exit (exitSuccess)
 import System.FilePath
@@ -16,35 +11,6 @@ import Control.Monad
 import Data.Set (Set)
 import qualified Data.Set as Set
 import qualified Data.ByteString.Lazy as B
-
-data TrackMetaData = TrackMetaData {
-                        artist  :: Maybe String
-                      , title   :: Maybe String
-                      , album   :: Maybe String
-                      , year    :: Maybe String
-                      , track   :: Maybe String
-                     } deriving (Generic, Show)
-
-instance ToJSON TrackMetaData where
-    toEncoding = genericToEncoding defaultOptions
-
-fromID3Tag :: ID3.Tag -> TrackMetaData
-fromID3Tag meta = TrackMetaData art titleField album year track
-    where
-        art = ID3.getArtist meta
-        titleField = ID3.getTitle meta
-        album = ID3.getAlbum meta
-        year = ID3.getYear meta
-        track = ID3.getTrack meta
-
-data TrackInfo = TrackInfo {
-                    url :: FilePath
-                --, duration :: Float TODO figure out how idiii or ID3v2 in general handles this
-                  , metaData :: Maybe TrackMetaData
-                 } deriving (Generic, Show)
-
-instance ToJSON TrackInfo where
-    toEncoding = genericToEncoding defaultOptions
 
 data PathStyle = RelativePath | AbsolutePath FilePath
     deriving Show
@@ -117,14 +83,6 @@ allowableExtensions = Set.fromList [".mp3"]
 filterByExtensions :: Set String -> [FilePath] -> [FilePath]
 filterByExtensions extensions = filter (\file -> Set.member (getExt file) extensions)
     where getExt = snd . splitExtension
-
-readTrackInfo :: Bool -> FilePath -> IO TrackInfo
-readTrackInfo pUseFname fp = do
-    tag <- ID3.readTag fp
-    let metadata = (fmap fromID3Tag) $ tag
-    --TODO fname as title
-    --TODO Path styling
-    return $ TrackInfo fp metadata
 
 main' :: AppConfig -> IO ()
 --CWD STDOUT case
---
+++ src/Wamphf.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE DeriveGeneric #-}
+
+module Wamphf (
+    TrackMetaData,
+    TrackInfo,
+    readTrackInfo
+  ) where
+
+import GHC.Generics
+import Data.Aeson
+import qualified ID3.Simple as ID3
+
+data TrackMetaData = TrackMetaData {
+                        artist  :: Maybe String
+                      , title   :: Maybe String
+                      , album   :: Maybe String
+                      , year    :: Maybe String
+                      , track   :: Maybe String
+                     } deriving (Generic, Show)
+
+instance ToJSON TrackMetaData where
+    toEncoding = genericToEncoding defaultOptions
+
+fromID3Tag :: ID3.Tag -> TrackMetaData
+fromID3Tag meta = TrackMetaData art titleField album year track
+    where
+        art = ID3.getArtist meta
+        titleField = ID3.getTitle meta
+        album = ID3.getAlbum meta
+        year = ID3.getYear meta
+        track = ID3.getTrack meta
+
+data TrackInfo = TrackInfo {
+                    url :: FilePath
+                --, duration :: Float TODO figure out how idiii or ID3v2 in general handles this
+                  , metaData :: Maybe TrackMetaData
+                 } deriving (Generic, Show)
+
+instance ToJSON TrackInfo where
+    toEncoding = genericToEncoding defaultOptions
+
+readTrackInfo :: Bool -> FilePath -> IO TrackInfo
+readTrackInfo pUseFname fp = do
+    tag <- ID3.readTag fp
+    let metadata = (fmap fromID3Tag) $ tag
+    --TODO fname as title
+    --TODO Path styling
+    return $ TrackInfo fp metadata
